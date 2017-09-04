6a2638f0a75654b4ecfa995cde253ef96b45699e
Dula <rdulmina@gmail.com>
2017-09-04 15:18:18 +0530

    Style sheet

---
+++ application/index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="css/SRScabsstyle.css">
+	</head>
+<body>
+	<div id="mainheder">
+		<div id="logo">
+			<a href="#">
+				SRS<span>Cabs</span>
+			</a>
+		</div>
+			
+	</div>
+</body>
+</html>
