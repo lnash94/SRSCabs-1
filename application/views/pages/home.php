6de83dce704243a23582e659101bc40c59a7dc65
lnash94 <sumudunissanke@gmail.com>
2017-09-05 22:51:57 +0530

    add header, footer and style sheet

---
+++ application/views/pages/home.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC
+ * Date: 9/5/2017
+ * Time: 10:29 PM
+ */
---
+++ application/views/template/header.php
@@ -0,0 +1,37 @@
+<?php
+?>
+
+<html>
+<head>
+    <title> Tech_Blog
+    </title>
+    <!--    <link rel="stylesheet" href="https://bootswatch.com/cyborg/bootstrap.min.css">-->
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/style.css">
+    <link rel="stylesheet" href="https://bootswatch.com/yeti/bootstrap.min.css">
+    <!--    <script src="http://cdn.ckeditor.com/4.7.2/basic/ckeditor.js"></script>-->
+    <script src="http://cdn.ckeditor.com/4.7.2/full/ckeditor.js"></script>
+
+</head>
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="<?php echo base_url();?>/">SRScabs</a>
+            </div>
+            <div id="navbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="<?php echo base_url();?>">Home</a></li>
+                    <li><a href="<?php echo base_url();?>/about">About</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+<!--<div id="mainheder">
+    <div id="logo">
+        <a href="#">
+            SRS<span>Cabs</span>
+        </a>
+    </div>
+--></div>
+<div class="container">
